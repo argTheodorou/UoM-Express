8f3b20c357429c63a6e004108599f7b970e6ef77
argir <argir@DESKTOP-DENDVDJ>
2019-05-23 21:11:59 +0300

    changed function so it parses values that are separated by commas

--- UOMExpress/src/NewSearchDialog.java
+++ UOMExpress/src/NewSearchDialog.java
@@ -26,7 +26,6 @@ public class NewSearchDialog extends JDialog {
 	private final JPanel contentPanel = new JPanel();
 	private JTextField searchTextField;
 
-	
 	public static void main(String[] args) {
 		try {
 			NewSearchDialog dialog = new NewSearchDialog();
@@ -38,142 +37,126 @@ public class NewSearchDialog extends JDialog {
 	}
 
 	public NewSearchDialog() throws FileNotFoundException {
-		setTitle("\u0391\u039D\u0391\u0396\u0397\u03A4\u0397\u03A3\u0397 \u0391\u03A0\u039F\u03A3\u03A4\u039F\u039B\u0397\u03A3");
+		setTitle(
+				"\u0391\u039D\u0391\u0396\u0397\u03A4\u0397\u03A3\u0397 \u0391\u03A0\u039F\u03A3\u03A4\u039F\u039B\u0397\u03A3");
 		setResizable(false);
 		setModal(true);
 		setIconImage(Toolkit.getDefaultToolkit().getImage(NewSearchDialog.class.getResource("/pics/box.png")));
 		setMinimumSize(new Dimension(800, 480));
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setBackground(new Color(57,62,70));
-	
+		contentPanel.setBackground(new Color(57, 62, 70));
+
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		JPanel panel = new JPanel();
-		panel.setBackground(new Color(57,62,70));
-		
+		panel.setBackground(new Color(57, 62, 70));
+
 		JLabel label_2 = new JLabel("");
 		label_2.setIcon(new ImageIcon(NewSearchDialog.class.getResource("/pics/logo400X400.png")));
 		GroupLayout gl_contentPanel = new GroupLayout(contentPanel);
-		gl_contentPanel.setHorizontalGroup(
-			gl_contentPanel.createParallelGroup(Alignment.LEADING)
+		gl_contentPanel.setHorizontalGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPanel.createSequentialGroup()
-					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 386, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(label_2, GroupLayout.PREFERRED_SIZE, 394, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-		);
-		gl_contentPanel.setVerticalGroup(
-			gl_contentPanel.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPanel.createSequentialGroup()
-					.addContainerGap()
-					.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
-						.addComponent(label_2, GroupLayout.DEFAULT_SIZE, 434, Short.MAX_VALUE)
-						.addComponent(panel, GroupLayout.PREFERRED_SIZE, 445, GroupLayout.PREFERRED_SIZE))
-					.addContainerGap())
-		);
-		
-		JLabel label = new JLabel("\u0391\u039D\u0391\u0396\u0397\u03A4\u0397\u03A3\u0397 \u0391\u03A0\u039F\u03A3\u03A4\u039F\u039B\u0397\u03A3");
+						.addComponent(panel, GroupLayout.PREFERRED_SIZE, 386, GroupLayout.PREFERRED_SIZE)
+						.addPreferredGap(ComponentPlacement.RELATED)
+						.addComponent(label_2, GroupLayout.PREFERRED_SIZE, 394, GroupLayout.PREFERRED_SIZE)
+						.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)));
+		gl_contentPanel.setVerticalGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPanel.createSequentialGroup().addContainerGap()
+						.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
+								.addComponent(label_2, GroupLayout.DEFAULT_SIZE, 434, Short.MAX_VALUE)
+								.addComponent(panel, GroupLayout.PREFERRED_SIZE, 445, GroupLayout.PREFERRED_SIZE))
+						.addContainerGap()));
+
+		JLabel label = new JLabel(
+				"\u0391\u039D\u0391\u0396\u0397\u03A4\u0397\u03A3\u0397 \u0391\u03A0\u039F\u03A3\u03A4\u039F\u039B\u0397\u03A3");
 		label.setForeground(Color.WHITE);
 		label.setFont(new Font("Calibri", Font.BOLD, 22));
-		
-		JLabel label_1 = new JLabel("\u03A0\u03BB\u03B7\u03BA\u03C4\u03C1\u03BF\u03BB\u03BF\u03B3\u03AE\u03C3\u03C4\u03B5 \u03AD\u03BD\u03B1\u03BD \u03BA\u03C9\u03B4\u03B9\u03BA\u03CC \u03C0\u03B1\u03C1\u03B1\u03BA\u03BF\u03BB\u03BF\u03CD\u03B8\u03B7\u03C3\u03B7\u03C2:");
+
+		JLabel label_1 = new JLabel(
+				"\u03A0\u03BB\u03B7\u03BA\u03C4\u03C1\u03BF\u03BB\u03BF\u03B3\u03AE\u03C3\u03C4\u03B5 \u03AD\u03BD\u03B1\u03BD \u03BA\u03C9\u03B4\u03B9\u03BA\u03CC \u03C0\u03B1\u03C1\u03B1\u03BA\u03BF\u03BB\u03BF\u03CD\u03B8\u03B7\u03C3\u03B7\u03C2:");
 		label_1.setForeground(Color.WHITE);
 		label_1.setFont(new Font("Calibri", Font.BOLD, 16));
-		
+
 		searchTextField = new JTextField();
 		searchTextField.setFont(new Font("Calibri", Font.BOLD, 35));
 		searchTextField.setColumns(10);
-		
+
 		JButton button = new JButton("");
 		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-			
+
 				try {
 					newSearch("C:/Users/argir/git/UoM-Express/UOMExpress/apostoles.txt", searchTextField.getText());
 				} catch (FileNotFoundException e) {
 					e.printStackTrace();
 				}
-				
+
 			}
 		});
 		button.setIcon(new ImageIcon(NewSearchDialog.class.getResource("/pics/magnifier.png")));
-		
-		JPanel panel_1 = new JPanel();
-		
+
+		JPanel resultPanel = new JPanel();
+
 		JButton button_1 = new JButton("\u039A\u03BB\u03B5\u03AF\u03C3\u03B9\u03BC\u03BF");
 		button_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				dispose();
 			}
 		});
-		
+
 		button_1.setFont(new Font("Calibri", Font.BOLD, 18));
 		GroupLayout gl_panel = new GroupLayout(panel);
-		gl_panel.setHorizontalGroup(
-			gl_panel.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel.createSequentialGroup()
-					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_panel.createSequentialGroup()
-							.addGap(74)
-							.addComponent(label))
-						.addGroup(gl_panel.createSequentialGroup()
-							.addGap(26)
-							.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
-								.addComponent(label_1)
+		gl_panel.setHorizontalGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup().addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel.createSequentialGroup().addGap(74).addComponent(label))
+						.addGroup(gl_panel.createSequentialGroup().addGap(26).addGroup(gl_panel.createParallelGroup(
+								Alignment.LEADING).addComponent(label_1)
 								.addGroup(gl_panel.createSequentialGroup()
-									.addGap(10)
-									.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING, false)
-										.addComponent(panel_1, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-										.addGroup(Alignment.LEADING, gl_panel.createSequentialGroup()
-											.addComponent(searchTextField, GroupLayout.PREFERRED_SIZE, 230, GroupLayout.PREFERRED_SIZE)
-											.addPreferredGap(ComponentPlacement.RELATED)
-											.addComponent(button, GroupLayout.PREFERRED_SIZE, 81, GroupLayout.PREFERRED_SIZE))))))
-						.addGroup(gl_panel.createSequentialGroup()
-							.addGap(90)
-							.addComponent(button_1, GroupLayout.PREFERRED_SIZE, 206, GroupLayout.PREFERRED_SIZE)))
-					.addContainerGap(28, Short.MAX_VALUE))
-		);
-		gl_panel.setVerticalGroup(
-			gl_panel.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(label, GroupLayout.PREFERRED_SIZE, 46, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(label_1, GroupLayout.PREFERRED_SIZE, 22, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+										.addGap(10)
+										.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING, false)
+												.addComponent(resultPanel, Alignment.LEADING, GroupLayout.DEFAULT_SIZE,
+														GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+												.addGroup(Alignment.LEADING, gl_panel.createSequentialGroup()
+														.addComponent(searchTextField, GroupLayout.PREFERRED_SIZE, 230,
+																GroupLayout.PREFERRED_SIZE)
+														.addPreferredGap(ComponentPlacement.RELATED)
+														.addComponent(button, GroupLayout.PREFERRED_SIZE, 81,
+																GroupLayout.PREFERRED_SIZE))))))
+						.addGroup(gl_panel.createSequentialGroup().addGap(90).addComponent(button_1,
+								GroupLayout.PREFERRED_SIZE, 206, GroupLayout.PREFERRED_SIZE)))
+						.addContainerGap(28, Short.MAX_VALUE)));
+		gl_panel.setVerticalGroup(gl_panel.createParallelGroup(Alignment.LEADING).addGroup(gl_panel
+				.createSequentialGroup().addContainerGap()
+				.addComponent(label, GroupLayout.PREFERRED_SIZE, 46, GroupLayout.PREFERRED_SIZE)
+				.addPreferredGap(ComponentPlacement.RELATED)
+				.addComponent(label_1, GroupLayout.PREFERRED_SIZE, 22, GroupLayout.PREFERRED_SIZE)
+				.addPreferredGap(ComponentPlacement.RELATED)
+				.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
 						.addComponent(searchTextField, GroupLayout.PREFERRED_SIZE, 41, GroupLayout.PREFERRED_SIZE)
 						.addComponent(button, GroupLayout.PREFERRED_SIZE, 41, GroupLayout.PREFERRED_SIZE))
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 233, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(button_1, GroupLayout.PREFERRED_SIZE, 42, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(21, Short.MAX_VALUE))
-		);
+				.addPreferredGap(ComponentPlacement.UNRELATED)
+				.addComponent(resultPanel, GroupLayout.PREFERRED_SIZE, 233, GroupLayout.PREFERRED_SIZE)
+				.addPreferredGap(ComponentPlacement.RELATED)
+				.addComponent(button_1, GroupLayout.PREFERRED_SIZE, 42, GroupLayout.PREFERRED_SIZE)
+				.addContainerGap(21, Short.MAX_VALUE)));
 		panel.setLayout(gl_panel);
 		contentPanel.setLayout(gl_contentPanel);
-		
-		
-		
-		
-       
-	}
-	
-	
-	public void newSearch  (String fileName,String code) throws FileNotFoundException{
-		
-	        Scanner scan = new Scanner(new File(fileName));
-	        while(scan.hasNext()){
-	            String line = scan.nextLine().toLowerCase().toString();
-	            if(line.contains(code)){
-	                System.out.println(line);
-	            }
-	        }
-	        
-	}
-	        
-	    
+
 	}
 
+	public void newSearch(String fileName, String code) throws FileNotFoundException {
 
+		Scanner scan = new Scanner(new File(fileName));
+		while (scan.hasNext()) {
+			String line = scan.nextLine().toLowerCase().toString();
+			if (line.contains(code)) {
+				String [] separatedStats = line.split(",");
+				String tk = separatedStats[2];
+				System.out.println(tk.toString());
+			}
+		}
+
+	}
+
+}
