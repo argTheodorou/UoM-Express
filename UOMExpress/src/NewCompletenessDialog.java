cb2c59c60c6b8de5dade1f8a7f7fbd220d4f8d4b
argir <argir@DESKTOP-DENDVDJ>
2019-06-11 20:05:38 +0300

    a new function, and a bunch of new changes and additions

--- UOMExpress/src/NewCompletenessDialog.java
+++ UOMExpress/src/NewCompletenessDialog.java
@@ -6,25 +6,33 @@ import java.awt.Font;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 import javax.swing.GroupLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.border.EmptyBorder;
-
+import javax.swing.JTextArea;
+import javax.swing.border.BevelBorder;
+import javax.swing.UIManager;
 
 public class NewCompletenessDialog extends JDialog {
+	
+	
 	private final JPanel contentPanel1 = new JPanel();
 	private final JPanel contentPanel = new JPanel();
+	String [] spaceUsedTable = null; 
 
-	public NewCompletenessDialog() {
+	public NewCompletenessDialog() throws FileNotFoundException {
 		setTitle(
 				"\u0395\u039B\u0395\u0393\u03A7\u039F\u03A3 \u03A0\u039B\u0397\u03A1\u039F\u03A4\u0397\u03A4\u0391\u03A3 \u039C\u0395\u03A3\u03A9\u039D");
 		setResizable(false);
@@ -34,6 +42,16 @@ public class NewCompletenessDialog extends JDialog {
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel1.setBackground(new Color(57, 62, 70));
+		
+		//textAreas to be used to display remaining ammount of space left
+		JTextArea capacityFieldShip = new JTextArea();
+		capacityFieldShip.setLineWrap(true);
+		JTextArea capacityFieldPlane = new JTextArea();
+		capacityFieldPlane.setForeground(Color.WHITE);
+		capacityFieldPlane.setFont(new Font("Calibri", Font.BOLD, 18));
+		capacityFieldPlane.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		capacityFieldPlane.setBackground(new Color(57, 62, 70));
+		capacityFieldPlane.setLineWrap(true);
 
 		contentPanel1.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel1, BorderLayout.CENTER);
@@ -57,10 +75,9 @@ public class NewCompletenessDialog extends JDialog {
 						.addContainerGap()));
 
 		JLabel label = new JLabel(
-				"\u0391\u039D\u0391\u0396\u0397\u03A4\u0397\u03A3\u0397 \u0391\u03A0\u039F\u03A3\u03A4\u039F\u039B\u0397\u03A3");
+				"\u039A\u0391\u03A4\u0391\u03A3\u03A4\u0391\u03A3\u0397 \u039C\u0395\u03A3\u03A9\u039D");
 		label.setForeground(Color.WHITE);
 		label.setFont(new Font("Calibri", Font.BOLD, 22));
-		
 
 		JButton button_1 = new JButton("\u039A\u03BB\u03B5\u03AF\u03C3\u03B9\u03BC\u03BF");
 		button_1.addActionListener(new ActionListener() {
@@ -70,25 +87,83 @@ public class NewCompletenessDialog extends JDialog {
 		});
 
 		button_1.setFont(new Font("Calibri", Font.BOLD, 18));
+		spaceUsedTable  = getSpaceUsed("C:/Users/argir/git/UoM-Express/UOMExpress/capacity.txt");
+		//calculating the numbers that the user will get to see
+		double missingShipSpace = 820000 - (Double.parseDouble(spaceUsedTable[0])) ;  
+		//calculates the space that has been already booked by packages on the SHIP
+		double remainingShipPercentage = (1- (missingShipSpace / 820000))*100;  
+		//makes a percentage out of it
+	
+		double missingPlaneSpace = 3500000 - (Double.parseDouble(spaceUsedTable[1])) ; 
+		//calculates the space that has been already booked by packages on the PLANE
+		double remainingPlanePercentage = (1- (missingPlaneSpace / 3500000))*100;  
+		//makes a percentage out of it
+		
+		
+		capacityFieldShip.setText("Χώρος που απομένει στο πλοίο: \n" 
+				+ ((820000-missingShipSpace)/100) + " κυβικά μέτρα"
+				+ "  ποσοστό: " + remainingShipPercentage 
+				
+				);
+		
+		capacityFieldPlane.setText("Χώρος που απομένει στο αεροπλάνο: \n");
+		
+		
+		
+		System.out.println(remainingShipPercentage );
+		System.out.println(remainingPlanePercentage);
+
+		JLabel shipPic = new JLabel("");
+		shipPic.setIcon(new ImageIcon(NewCompletenessDialog.class.getResource("/pics/plane 120x120 100%.png")));
+
+		JLabel planePic = new JLabel("");
+		planePic.setIcon(new ImageIcon(NewCompletenessDialog.class.getResource("/pics/plane 120x120 100%.png")));
+		
+		
+		capacityFieldShip.setForeground(Color.WHITE);
+		capacityFieldShip.setFont(new Font("Calibri", Font.BOLD, 18));
+		capacityFieldShip.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		capacityFieldShip.setBackground(new Color(57, 62, 70));
+		
+	
 		GroupLayout gl_panel = new GroupLayout(panel);
 		gl_panel.setHorizontalGroup(
 			gl_panel.createParallelGroup(Alignment.TRAILING)
 				.addGroup(gl_panel.createSequentialGroup()
-					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_panel.createSequentialGroup()
-							.addGap(74)
-							.addComponent(label))
-						.addGroup(gl_panel.createSequentialGroup()
-							.addGap(90)
-							.addComponent(button_1, GroupLayout.PREFERRED_SIZE, 206, GroupLayout.PREFERRED_SIZE)))
-					.addGap(20))
+					.addGap(89)
+					.addComponent(label)
+					.addContainerGap(116, Short.MAX_VALUE))
+				.addGroup(gl_panel.createSequentialGroup()
+					.addContainerGap(93, Short.MAX_VALUE)
+					.addComponent(button_1, GroupLayout.PREFERRED_SIZE, 206, GroupLayout.PREFERRED_SIZE)
+					.addGap(87))
+				.addGroup(gl_panel.createSequentialGroup()
+					.addContainerGap(14, Short.MAX_VALUE)
+					.addComponent(capacityFieldShip, GroupLayout.PREFERRED_SIZE, 171, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(capacityFieldPlane, GroupLayout.PREFERRED_SIZE, 162, GroupLayout.PREFERRED_SIZE)
+					.addGap(33))
+				.addGroup(Alignment.LEADING, gl_panel.createSequentialGroup()
+					.addGap(36)
+					.addComponent(shipPic)
+					.addGap(52)
+					.addComponent(planePic)
+					.addContainerGap(58, Short.MAX_VALUE))
 		);
 		gl_panel.setVerticalGroup(
-			gl_panel.createParallelGroup(Alignment.LEADING)
+			gl_panel.createParallelGroup(Alignment.TRAILING)
 				.addGroup(gl_panel.createSequentialGroup()
 					.addContainerGap()
 					.addComponent(label, GroupLayout.PREFERRED_SIZE, 46, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED, 334, Short.MAX_VALUE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+						.addComponent(planePic, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(shipPic))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
+						.addComponent(capacityFieldShip, GroupLayout.PREFERRED_SIZE, 182, GroupLayout.PREFERRED_SIZE)
+						.addComponent(capacityFieldPlane, GroupLayout.PREFERRED_SIZE, 179, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED)
 					.addComponent(button_1, GroupLayout.PREFERRED_SIZE, 42, GroupLayout.PREFERRED_SIZE)
 					.addGap(21))
 		);
@@ -96,4 +171,16 @@ public class NewCompletenessDialog extends JDialog {
 		contentPanel1.setLayout(gl_contentPanel);
 	}
 
+	
+	//returns a table with the remainder of the available space in our shipping methods
+	private String[] getSpaceUsed(String fileName) throws FileNotFoundException {
+
+		Scanner scan = new Scanner(new File(fileName));
+		String[] separatedByCommasArray = null;
+
+		String line = scan.nextLine().toString();
+
+		separatedByCommasArray = line.split(",");  //pushes the  line into an array. Every String is put in a different field. starting at [0]
+		return separatedByCommasArray;
+	}
 }
